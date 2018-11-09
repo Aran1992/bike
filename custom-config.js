9a7ed51ff31935ef7e2707cf87fe5cad0551a19b
james <sunfloor1976@qq.com>
2018-11-09 15:50:31 +0800

    脚本、场景 资源调整

--- custom-config.js
+++ custom-config.js
@@ -1,17 +1,17 @@
-﻿// 自行车速度
-config.bikeVelocity = 20;
+﻿// 自行车速度（基础：20）
+config.bikeVelocity = 40;
 
-// 重力
+// 重力（基础：-200）
 config.gravity = -200;
 
-// 跳跃爆发力
+// 跳跃爆发力（基础：5000）
 config.jumpForce = 5000;
 
 // 地图配置
 config.mapList = [
     {
         // 显示给人看的名字
-        showName: "Test",
+        showName: "基础场景",
         // scene文件的名字
         sceneName: "Test",
         // 地图所需的图片路径（看描述还是不明白是啥的话 可以直接打开对应的文件看看）
@@ -27,12 +27,14 @@ config.mapList = [
                 "images/Scorpius/TextureScorpiusBackground03.png",
             ]
         },
-        horizontalParallaxDepth: [0.75, 0.5, 0.25, 0],
-        verticalParallaxDepth: [0.75, 0.5, 0.25, 0],
+		// 视差背景系数：左右
+        horizontalParallaxDepth: [0.9, 0.8, 0.5, 0],
+		// 视差背景系数：上下
+        verticalParallaxDepth: [1, 1, 0.8, 0],
     },
     {
         // 显示给人看的名字
-        showName: "Test2",
+        showName: "上坡场景",
         // scene文件的名字
         sceneName: "Test2",
         // 地图所需的图片路径（看描述还是不明白是啥的话 可以直接打开对应的文件看看）
@@ -48,7 +50,32 @@ config.mapList = [
                 "images/Taurus/TextureTaurusBackground03.png",
             ]
         },
-        horizontalParallaxDepth: [0.75, 0.5, 0.25, 0],
-        verticalParallaxDepth: [0.75, 0.5, 0.25, 0],
+		// 视差背景系数左右
+        horizontalParallaxDepth: [0.9, 0.8, 0.5, 0],
+		// 视差背景系数：上下
+        verticalParallaxDepth: [1, 0.9, 0.8, 0],
+    },
+    {
+        // 显示给人看的名字
+        showName: "下坡场景",
+        // scene文件的名字
+        sceneName: "Test3",
+        // 地图所需的图片路径（看描述还是不明白是啥的话 可以直接打开对应的文件看看）
+        texture: {
+            // 侧边
+            side: "images/Taurus/TextureTaurusRoad01SideSkin.png",
+            // 底部
+            top: "images/Taurus/TextureTaurusRoad01TopSkin.png",
+            // 背景 有多少层背景就配置多少张
+            bg: [
+                "images/Taurus/TextureTaurusBackground01.png",
+                "images/Taurus/TextureTaurusBackground02.png",
+                "images/Taurus/TextureTaurusBackground03.png",
+            ]
+        },
+		// 视差背景系数左右
+        horizontalParallaxDepth: [0.9, 0.8, 0.5, 0],
+		// 视差背景系数：上下
+        verticalParallaxDepth: [1, 0.9, 0.8, 0],
     },
 ];
