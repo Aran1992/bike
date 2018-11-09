cb7737c9dfc69bb3f82510c49400ee63cfef12f6
james <sunfloor1976@qq.com>
2018-11-09 17:55:31 +0800

    第1版对外资源调整更新

--- custom-config.js
+++ custom-config.js
@@ -1,5 +1,5 @@
 ﻿// 自行车速度（基础：20）
-config.bikeVelocity = 40;
+config.bikeVelocity = 20;
 
 // 重力（基础：-200）
 config.gravity = -200;
@@ -17,25 +17,25 @@ config.mapList = [
         // 地图所需的图片路径（看描述还是不明白是啥的话 可以直接打开对应的文件看看）
         texture: {
             // 侧边
-            side: "images/Scorpius/TextureScorpiusRoad01SideSkin.png",
+            side: "images/Taurus/TextureTaurusRoad01SideSkin.png",
             // 底部
-            top: "images/Scorpius/TextureScorpiusRoad01TopSkin.png",
+            top: "images/Taurus/TextureTaurusRoad01TopSkin.png",
             // 背景 有多少层背景就配置多少张
             bg: [
-                "images/Scorpius/TextureScorpiusBackground01.png",
-                "images/Scorpius/TextureScorpiusBackground01.png",
-                "images/Scorpius/TextureScorpiusBackground02.png",
+                "images/Taurus/TextureTaurusBackground01.png",
+                "images/Taurus/TextureTaurusBackground02.png",
+                "images/Taurus/TextureTaurusBackground03.png",
             ]
         },
 		// 视差背景系数：左右
         horizontalParallaxDepth: [0.9, 0.8, 0.5, 0],
 		// 视差背景系数：上下
-        verticalParallaxDepth: [1, 1, 0.8, 0],
-        // 自行车速度
-        bikeVelocity: 40,
-        // 重力
+        verticalParallaxDepth: [1, 0.9, 0.8, 0],
+        // 自行车速度（基础：20）
+        bikeVelocity: 20,
+        // 重力（基础：-200）
         gravity: -200,
-        // 跳跃爆发力
+        // 跳跃爆发力（基础：5000）
         jumpForce: 5000,
     },
     {
@@ -46,25 +46,25 @@ config.mapList = [
         // 地图所需的图片路径（看描述还是不明白是啥的话 可以直接打开对应的文件看看）
         texture: {
             // 侧边
-            side: "images/Taurus/TextureTaurusRoad01SideSkin.png",
+            side: "images/Aries/TextureAriesRoad01SideSkin.png",
             // 底部
-            top: "images/Taurus/TextureTaurusRoad01TopSkin.png",
+            top: "images/Aries/TextureAriesRoad01TopSkin.png",
             // 背景 有多少层背景就配置多少张
             bg: [
-                "images/Taurus/TextureTaurusBackground01.png",
-                "images/Taurus/TextureTaurusBackground02.png",
-                "images/Taurus/TextureTaurusBackground03.png",
+                "images/Aries/TextureAriesBackground01.png",
+                "images/Aries/TextureAriesBackground02.png",
+                "images/Aries/TextureAriesBackground03.png",
             ]
         },
 		// 视差背景系数左右
         horizontalParallaxDepth: [0.9, 0.8, 0.5, 0],
 		// 视差背景系数：上下
         verticalParallaxDepth: [1, 0.9, 0.8, 0],
-        // 自行车速度
+        // 自行车速度（基础：20）
         bikeVelocity: 20,
-        // 重力
+        // 重力（基础：-200）
         gravity: -200,
-        // 跳跃爆发力
+        // 跳跃爆发力（基础：5000）
         jumpForce: 5000,
     },
     {
@@ -75,25 +75,83 @@ config.mapList = [
         // 地图所需的图片路径（看描述还是不明白是啥的话 可以直接打开对应的文件看看）
         texture: {
             // 侧边
-            side: "images/Taurus/TextureTaurusRoad01SideSkin.png",
+            side: "images/Capricorn/TextureCapricornRoad01SideSkin.png",
             // 底部
-            top: "images/Taurus/TextureTaurusRoad01TopSkin.png",
+            top: "images/Capricorn/TextureCapricornRoad01TopSkin.png",
             // 背景 有多少层背景就配置多少张
             bg: [
-                "images/Taurus/TextureTaurusBackground01.png",
-                "images/Taurus/TextureTaurusBackground02.png",
-                "images/Taurus/TextureTaurusBackground03.png",
+                "images/Capricorn/TextureCapricornBackground01.png",
+                "images/Capricorn/TextureCapricornBackground02.png",
+                "images/Capricorn/TextureCapricornBackground03.png",
             ]
         },
 		// 视差背景系数左右
         horizontalParallaxDepth: [0.9, 0.8, 0.5, 0],
 		// 视差背景系数：上下
         verticalParallaxDepth: [1, 0.9, 0.8, 0],
-        // 自行车速度
+        // 自行车速度（基础：20）
         bikeVelocity: 20,
-        // 重力
+        // 重力（基础：-200）
         gravity: -200,
-        // 跳跃爆发力
+        // 跳跃爆发力（基础：5000）
+        jumpForce: 5000,
+    },
+    {
+        // 显示给人看的名字
+        showName: "基础场景 加速度",
+        // scene文件的名字
+        sceneName: "Test4",
+        // 地图所需的图片路径（看描述还是不明白是啥的话 可以直接打开对应的文件看看）
+        texture: {
+            // 侧边
+            side: "images/Scorpius/TextureScorpiusRoad01SideSkin.png",
+            // 底部
+            top: "images/Scorpius/TextureScorpiusRoad01TopSkin.png",
+            // 背景 有多少层背景就配置多少张
+            bg: [
+                "images/Scorpius/TextureScorpiusBackground01.png",
+                "images/Scorpius/TextureScorpiusBackground02.png",
+                "images/Scorpius/TextureScorpiusBackground03.png",
+            ]
+        },
+		// 视差背景系数左右
+        horizontalParallaxDepth: [0.9, 0.8, 0.5, 0],
+		// 视差背景系数：上下
+        verticalParallaxDepth: [1, 1, 0.8, 0],
+        // 自行车速度（基础：20）
+        bikeVelocity: 40,
+        // 重力（基础：-200）
+        gravity: -200,
+        // 跳跃爆发力（基础：5000）
+        jumpForce: 5000,
+    },
+    {
+        // 显示给人看的名字
+        showName: "基础场景 低重力",
+        // scene文件的名字
+        sceneName: "Test5",
+        // 地图所需的图片路径（看描述还是不明白是啥的话 可以直接打开对应的文件看看）
+        texture: {
+            // 侧边
+            side: "images/Aquarius/TextureAquariusRoad01SideSkin.png",
+            // 底部
+            top: "images/Aquarius/TextureAquariusRoad01TopSkin.png",
+            // 背景 有多少层背景就配置多少张
+            bg: [
+                "images/Aquarius/TextureAquariusBackground01.png",
+                "images/Aquarius/TextureAquariusBackground02.png",
+                "images/Aquarius/TextureAquariusBackground03.png",
+            ]
+        },
+		// 视差背景系数左右
+        horizontalParallaxDepth: [0.9, 0.8, 0.5, 0],
+		// 视差背景系数：上下
+        verticalParallaxDepth: [1, 1, 0.8, 0],
+        // 自行车速度（基础：20）
+        bikeVelocity: 20,
+        // 重力（基础：-200）
+        gravity: -50,
+        // 跳跃爆发力（基础：5000）
         jumpForce: 5000,
     },
 ];
