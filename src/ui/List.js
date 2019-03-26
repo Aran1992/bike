c37c260d1963600d6b92e935bcd04b6e350191d1
Aran1992 <2572261667@qq.com>
2019-03-26 21:20:51 +0800

    列表控件支持水平排列

--- src/ui/List.js
+++ src/ui/List.js
@@ -3,29 +3,151 @@ import UIHelper from "./UIHelper";
 import Config from "../config";
 
 export default class List {
-    constructor({root, initItemFunc, updateItemFunc, count, isStatic}) {
+    constructor({root, initItemFunc, updateItemFunc, count, isStatic, isHorizontal}) {
         this.itemTable = {};
         this.cacheItemList = [];
+        this.count = count;
+
         this.root = root;
+        this.root.hitArea = new Rectangle(0, 0, this.root.mywidth, this.root.myheight);
+
+        this.container = this.root.addChild(new Container());
+
+        this.item = root.children[0];
+        this.item.visible = false;
+        this.initItem = initItemFunc;
+        this.updateItem = updateItemFunc;
+
         this.createMask();
+
         if (!isStatic) {
             this.bindListener();
         }
-        this.item = root.children[0];
-        this.item.visible = false;
-        this.container = new Container();
-        this.root.addChild(this.container);
-        this.initItem = initItemFunc;
-        this.updateItem = updateItemFunc;
-        this.count = count;
-        this.countPerLine = Math.ceil(this.root.mywidth / this.item.mywidth);
-        this.maxY = 0;
-        this.minY = this.root.myheight - Math.ceil(this.count / this.countPerLine) * this.item.myheight;
-        if (this.minY > this.maxY) {
-            this.minY = 0;
+
+        this.initDirectionRelatedVariable(isHorizontal);
+
+        this.countPerLine = Math.ceil(this.lineLength / this.itemLineLength);
+        this.maxOffset = 0;
+        this.minOffset = this.viewLength - Math.ceil(this.count / this.countPerLine) * this.itemLength;
+        if (this.minOffset > this.maxOffset) {
+            this.minOffset = 0;
         }
-        this.root.hitArea = new Rectangle(0, 0, this.root.mywidth, this.root.myheight);
+
         this.update();
+    }
+
+    initDirectionRelatedVariable(isHorizontal) {
+        this.isHorizontal = isHorizontal;
+        if (isHorizontal) {
+            this.lineLength = this.root.myheight;
+            this.viewLength = this.root.mywidth;
+            this.itemLineLength = this.item.myheight;
+            this.itemLength = this.item.mywidth;
+            this.moveAxis = "x";
+        } else {
+            this.lineLength = this.root.mywidth;
+            this.viewLength = this.root.myheight;
+            this.itemLineLength = this.item.mywidth;
+            this.itemLength = this.item.myheight;
+            this.moveAxis = "y";
+        }
+    }
+
+    getItemPosition(index) {
+        if (this.isHorizontal) {
+            let column = Math.floor(index / this.countPerLine),
+                row = index % this.countPerLine;
+            return [
+                column * this.itemLength,
+                row * this.itemLineLength
+            ];
+        } else {
+            let column = index % this.countPerLine,
+                row = Math.floor(index / this.countPerLine);
+            return [
+                column * this.itemLineLength,
+                row * this.itemLength
+            ];
+        }
+    }
+
+    createMask() {
+        let {x, y} = this.root.getGlobalPosition();
+        this.root.mask = new Graphics()
+            .beginFill()
+            .drawRect(x, y, this.root.mywidth, this.root.myheight)
+            .endFill();
+    }
+
+    createItem() {
+        let item = this.cacheItemList.pop();
+        if (item === undefined) {
+            item = UIHelper.uiClone(this.item);
+            this.container.addChild(item);
+            if (this.initItem) {
+                this.initItem(item);
+            }
+        }
+        return item;
+    }
+
+    bindListener() {
+        this.root.buttonMode = true;
+        this.root.interactive = true;
+        this.root.on("pointerdown", this.onTouchStart.bind(this));
+        this.root.on("pointermove", this.onTouchMove.bind(this));
+        this.root.on("pointerup", this.onTouchEnd.bind(this));
+        this.root.on("pointerupoutside", this.onTouchEnd.bind(this));
+    }
+
+    onTouchStart(event) {
+        this.touching = true;
+        this.lastTouchOffset = event.data.global[this.moveAxis];
+        cancelAnimationFrame(this.recoverAnimationID);
+    }
+
+    onTouchMove(event) {
+        if (this.touching) {
+            let curTouchOffset = event.data.global[this.moveAxis];
+            let moveOffset = curTouchOffset - this.lastTouchOffset;
+            this.lastTouchOffset = curTouchOffset;
+            let newOffset = this.container[this.moveAxis] + moveOffset;
+            if (newOffset > this.maxOffset || newOffset < this.minOffset) {
+                newOffset = this.container[this.moveAxis] + moveOffset / Config.listResistance;
+            }
+            this.container[this.moveAxis] = newOffset;
+            this.update();
+        }
+    }
+
+    onTouchEnd() {
+        this.touching = false;
+        if (this.container[this.moveAxis] > this.maxOffset || this.container[this.moveAxis] < this.minOffset) {
+            let totalTime = 250;
+            let lastTime = (new Date()).getTime();
+            let totalMove = (this.container[this.moveAxis] > this.maxOffset ? this.maxOffset : this.minOffset) - this.container[this.moveAxis];
+            let handler = () => {
+                let curTime = (new Date()).getTime();
+                if (this.container[this.moveAxis] > this.minOffset) {
+                    this.container[this.moveAxis] += (curTime - lastTime) / totalTime * totalMove;
+                    if (this.container[this.moveAxis] <= this.maxOffset) {
+                        this.container[this.moveAxis] = this.maxOffset;
+                    } else {
+                        this.recoverAnimationID = requestAnimationFrame(handler);
+                    }
+                } else if (this.container[this.moveAxis] < this.maxOffset) {
+                    this.container[this.moveAxis] += (curTime - lastTime) / totalTime * totalMove;
+                    if (this.container[this.moveAxis] >= this.minOffset) {
+                        this.container[this.moveAxis] = this.minOffset;
+                    } else {
+                        this.recoverAnimationID = requestAnimationFrame(handler);
+                    }
+                }
+                this.update();
+                lastTime = curTime;
+            };
+            handler();
+        }
     }
 
     calcStartAndEnd(offset, itemLength, viewLength) {
@@ -43,11 +165,11 @@ export default class List {
         if (end >= this.count) {
             end = this.count - 1;
         }
-        return {startLine, endLine, start, end};
+        return {start, end};
     }
 
     update() {
-        let {start, end} = this.calcStartAndEnd(-this.container.y, this.item.myheight, this.root.myheight);
+        let {start, end} = this.calcStartAndEnd(-this.container[this.moveAxis], this.itemLength, this.viewLength);
         for (let index in this.itemTable) {
             if (this.itemTable.hasOwnProperty(index)) {
                 if (index < start || index > end) {
@@ -59,10 +181,9 @@ export default class List {
         }
         for (let index = start; index <= end; index++) {
             if (this.itemTable[index] === undefined) {
-                let item = this.getItem();
+                let item = this.createItem();
                 this.itemTable[index] = item;
-                let column = index % this.countPerLine, row = Math.floor(index / this.countPerLine);
-                item.position.set(column * this.item.mywidth, row * this.item.myheight);
+                item.position.set(...this.getItemPosition(index));
                 item.visible = true;
                 this.updateItem(item, index);
             }
@@ -75,84 +196,5 @@ export default class List {
                 this.updateItem(this.itemTable[index], parseInt(index));
             }
         }
-    }
-
-    getItem() {
-        let item = this.cacheItemList.pop();
-        if (item === undefined) {
-            item = UIHelper.uiClone(this.item);
-            this.container.addChild(item);
-            if (this.initItem) {
-                this.initItem(item);
-            }
-        }
-        return item;
-    }
-
-    onTouchStart(event) {
-        this.touching = true;
-        this.lastY = event.data.global.y;
-        cancelAnimationFrame(this.recoverAnimationID);
-    }
-
-    onTouchMove(event) {
-        if (this.touching) {
-            let moveY = event.data.global.y - this.lastY;
-            this.lastY = event.data.global.y;
-            let newY = this.container.y + moveY;
-            if (newY > this.maxY || newY < this.minY) {
-                newY = this.container.y + moveY / Config.listResistance;
-            }
-            this.container.y = newY;
-            this.update();
-        }
-    }
-
-    onTouchEnd() {
-        this.touching = false;
-        if (this.container.y > this.maxY || this.container.y < this.minY) {
-            let totalTime = 250;
-            let lastTime = (new Date()).getTime();
-            let totalMove = (this.container.y > this.maxY ? this.maxY : this.minY) - this.container.y;
-            let handler = () => {
-                let curTime = (new Date()).getTime();
-                if (this.container.y > this.minY) {
-                    this.container.y += (curTime - lastTime) / totalTime * totalMove;
-                    if (this.container.y <= this.maxY) {
-                        this.container.y = this.maxY;
-                    } else {
-                        this.recoverAnimationID = requestAnimationFrame(handler);
-                    }
-                } else if (this.container.y < this.maxY) {
-                    this.container.y += (curTime - lastTime) / totalTime * totalMove;
-                    if (this.container.y >= this.minY) {
-                        this.container.y = this.minY;
-                    } else {
-                        this.recoverAnimationID = requestAnimationFrame(handler);
-                    }
-                }
-                this.update();
-                lastTime = curTime;
-            };
-            handler();
-        }
-    }
-
-    createMask() {
-        let mask = new Graphics();
-        mask.beginFill();
-        let {x, y} = this.root.getGlobalPosition();
-        mask.drawRect(x, y, this.root.mywidth, this.root.myheight);
-        mask.endFill();
-        this.root.mask = mask;
-    }
-
-    bindListener() {
-        this.root.buttonMode = true;
-        this.root.interactive = true;
-        this.root.on("pointerdown", this.onTouchStart.bind(this));
-        this.root.on("pointermove", this.onTouchMove.bind(this));
-        this.root.on("pointerup", this.onTouchEnd.bind(this));
-        this.root.on("pointerupoutside", this.onTouchEnd.bind(this));
     }
 }
